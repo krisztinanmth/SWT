b0f09d11e9bacb58bf2f6e250c6afebbc7b2220c
Nemeth Krisztina <knemeth@b2international.com>
2019-03-06 13:36:08 +0100

    chore(SWT_intro_tutorial): create EventListener class

--- SWT_intro_tutorial/src/EventListener.java
+++ SWT_intro_tutorial/src/EventListener.java
@@ -1,4 +1,14 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
 
 public class EventListener {
+	
+	
+	Shell shell = new Shell();
+	
+	Button button = new Button(shell, SWT.PUSH);
+	
+
 
 }
