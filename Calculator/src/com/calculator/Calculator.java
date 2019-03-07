62b2a055f73414379fd4590e91462f097da5afaf
Nemeth Krisztina <knemeth@b2international.com>
2019-03-07 15:59:52 +0100

    feat(Calculator): read numbers from two text inputs and print result to console when user clicks '+' button

--- Calculator/src/com/calculator/Calculator.java
+++ Calculator/src/com/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.calculator;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
@@ -8,25 +10,32 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class Calculator {
-
+	
+	
 	public static void main(String[] args) {
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
+		
 				
 		shell.setLayout(new FillLayout());
 		
-		Text firstNumber = new Text(shell, SWT.CENTER);
-		firstNumber.setText("please provide first number");
+		Text textFirstNumber = new Text(shell, SWT.BORDER);
+		Text textSecondNumber = new Text(shell, SWT.BORDER);
+			
 		
 		Button button = new Button(shell, SWT.PUSH);
+		button.setSize(20, 20);
 		button.setText("+");
 		
-		Text secondNumber = new Text(shell, SWT.CENTER);
-		secondNumber.setText("please provide second number");
 		
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println(Integer.parseInt(textFirstNumber.getText()) + Integer.parseInt(textSecondNumber.getText()));
+			}
+		});
 		
-
 		shell.pack();
 		shell.open();
 		
