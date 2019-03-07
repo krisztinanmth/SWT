0f1090d0431200ed7ec109406047c3ced3cf0103
Nemeth Krisztina <knemeth@b2international.com>
2019-03-07 11:49:13 +0100

    chore(FirstSWT_plugin_Project): create simpleShell class

--- FirstSWT_plugin_Project/src/com/first/swt/plugin/project/SimpleShell.java
+++ FirstSWT_plugin_Project/src/com/first/swt/plugin/project/SimpleShell.java
@@ -1,5 +1,11 @@
 package com.first.swt.plugin.project;
 
-public class Test {
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class SimpleShell {
+	
+	Display display = new Display();
+	Shell shell = new Shell(display);
 
 }
