ee57ca6469ca8fce2104fc87644fc8b5402685cd
Nemeth Krisztina <knemeth@b2international.com>
2019-03-07 17:51:47 +0100

    feat(Calculator): result label displays result of addition

--- Calculator/src/com/calculator/Calculator.java
+++ Calculator/src/com/calculator/Calculator.java
@@ -8,10 +8,12 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 public class Calculator {
+	
 	
 	
 	public static void main(String[] args) {
@@ -19,12 +21,12 @@ public class Calculator {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setLayout(new GridLayout(1, true));
+		shell.setSize(300, 300);
 		
 //		Composite mainComposite = new Composite(shell, SWT.NONE);
 		Composite editorComposite = new Composite(shell, SWT.NONE);
-		editorComposite.setLayout(new GridLayout(3, false));
+		editorComposite.setLayout(new GridLayout(5, false));
 					
-		
 		Text textFirstNumber = new Text(editorComposite, SWT.BORDER);
 		
 		Button addButton = new Button(editorComposite, SWT.NONE); // SWT.DROP-DOWN
@@ -32,8 +34,11 @@ public class Calculator {
 		
 		Text textSecondNumber = new Text(editorComposite, SWT.BORDER);
 		
-//		Label 
+		Label equalLabel = new Label(editorComposite, SWT.BORDER);
+		equalLabel.setText("=");
 		
+		Label resultLabel = new Label(editorComposite, SWT.BORDER_SOLID);
+		resultLabel.setText("-");
 		
 		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
 		gridData.horizontalSpan = 2;
@@ -45,7 +50,9 @@ public class Calculator {
 		calculateButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				System.out.println(Integer.parseInt(textFirstNumber.getText()) + Integer.parseInt(textSecondNumber.getText()));
+				int result = Integer.parseInt(textFirstNumber.getText()) + Integer.parseInt(textSecondNumber.getText());
+//				System.out.println(Integer.parseInt(textFirstNumber.getText()) + Integer.parseInt(textSecondNumber.getText()));
+				resultLabel.setText(String.valueOf(result));
 			}
 		});
 		
