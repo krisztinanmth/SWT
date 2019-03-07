5232f3f76a22e15a2a50a7b5476953bc39696ef0
Nemeth Krisztina <knemeth@b2international.com>
2019-03-07 17:09:50 +0100

    feat(Calculator): add calculate button

--- Calculator/src/com/calculator/Calculator.java
+++ Calculator/src/com/calculator/Calculator.java
@@ -3,8 +3,10 @@ package com.calculator;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
@@ -16,27 +18,38 @@ public class Calculator {
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
+		shell.setLayout(new GridLayout(1, true));
 		
-				
-		shell.setLayout(new FillLayout());
+//		Composite mainComposite = new Composite(shell, SWT.NONE);
+		Composite editorComposite = new Composite(shell, SWT.NONE);
+		editorComposite.setLayout(new GridLayout(3, false));
+					
 		
-		Text textFirstNumber = new Text(shell, SWT.BORDER);
-		Text textSecondNumber = new Text(shell, SWT.BORDER);
-			
+		Text textFirstNumber = new Text(editorComposite, SWT.BORDER);
 		
-		Button button = new Button(shell, SWT.PUSH);
-		button.setSize(20, 20);
-		button.setText("+");
+		Button addButton = new Button(editorComposite, SWT.NONE); // SWT.DROP-DOWN
+		addButton.setText("+");
+		
+		Text textSecondNumber = new Text(editorComposite, SWT.BORDER);
+		
+//		Label 
 		
 		
-		button.addSelectionListener(new SelectionAdapter() {
+		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		gridData.horizontalSpan = 2;
+
+		Button calculateButton = new Button(shell, SWT.PUSH);
+		calculateButton.setLayoutData(gridData);
+		calculateButton.setText("calculate result");
+		
+		calculateButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				System.out.println(Integer.parseInt(textFirstNumber.getText()) + Integer.parseInt(textSecondNumber.getText()));
 			}
 		});
 		
-		shell.pack();
+//		shell.pack(); -> this is why the window was small
 		shell.open();
 		
 		while (!shell.isDisposed()) {
