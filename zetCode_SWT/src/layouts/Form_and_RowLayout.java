550f8d9f0eab058d0630b368a5ad7118db67d439
Nemeth Krisztina <knemeth@b2international.com>
2019-03-08 07:10:44 +0100

    chore(Form_and_RowLayout): add useful comments

--- zetCode_SWT/src/layouts/Form_and_RowLayout.java
+++ zetCode_SWT/src/layouts/Form_and_RowLayout.java
@@ -21,11 +21,10 @@ public class Form_and_RowLayout {
 
 	private void initUI(Display display) {
 		Shell shell = new Shell(display, SWT.SHELL_TRIM | SWT.CENTER);
-		shell.setLayout(new FormLayout());
+		shell.setLayout(new FormLayout()); // FormLayout is set to be the shell's main layout manager
 		
-		Label lbl = new Label(shell, SWT.LEFT);
+		Label lbl = new Label(shell, SWT.LEFT); // lbl widget is attached at the upper-left corner of the window
 		lbl.setText("name: ");
-		
 		FormData data1 = new FormData();
 		data1.left = new FormAttachment(0, 5);
 		data1.top = new FormAttachment(0, 10);
@@ -33,13 +32,13 @@ public class Form_and_RowLayout {
 		
 		Text text = new Text(shell, SWT.SINGLE);
 		FormData data2 = new FormData();
-		data2.left = new FormAttachment(lbl, 15);
+		data2.left = new FormAttachment(lbl, 15); // left side of text is placed relatively to the lbl
 		data2.top = new FormAttachment(0, 10);
 		data2.right = new FormAttachment(100, -5);
 		text.setLayoutData(data2);
 		
-		Composite com = new Composite(shell, SWT.NONE);
-		RowLayout rowLayout = new RowLayout();
+		Composite com = new Composite(shell, SWT.NONE); // composite created -> for ok and close btns
+		RowLayout rowLayout = new RowLayout(); // easier to use RowLayout for buttons, then to organize them directly with the FormLayout
 		com.setLayout(rowLayout);
 		
 		Button okBtn = new Button(com, SWT.PUSH);
@@ -50,11 +49,15 @@ public class Form_and_RowLayout {
 		closeBtn.setText("close");
 		closeBtn.setLayoutData(new RowData(80, 30));
 		
+		// The Composite itself is placed at the bottom of the window with the FormLayout.
+		// Negative values are offsets from the neighbouring widgets or window borders.
 		FormData data3 = new FormData();
 		data3.bottom = new FormAttachment(100, -5);
 		data3.right = new FormAttachment(100, 0);
 		com.setLayoutData(data3);
 		
+		// Finally, the main Text widget is created. It takes the most of the window area.
+		//The width and height properties specify the initial preferred size of the control.
 		Text mainText = new Text(shell, SWT.MULTI | SWT.BORDER);
 		FormData data4 = new FormData();
 		data4.width = 250;
