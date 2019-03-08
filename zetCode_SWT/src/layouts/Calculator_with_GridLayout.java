d58b9ca9f2e513e35bebd2308561844c4c8b9c5d
Nemeth Krisztina <knemeth@b2international.com>
2019-03-08 07:41:16 +0100

    feat(Calculator_with_GridLayout): create Calculator_with_GridLayout class

---
+++ zetCode_SWT/src/layouts/Calculator_with_GridLayout.java
@@ -0,0 +1,68 @@
+package layouts;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class Calculator_with_GridLayout {
+	
+	public Calculator_with_GridLayout(Display display) {
+		initUI(display);
+	}
+
+	private void initUI(Display display) {
+		Shell shell = new Shell(display, SWT.DIALOG_TRIM | SWT.CENTER);
+		
+		GridLayout gL = new GridLayout(4, true);
+		gL.marginHeight = 5;
+		shell.setLayout(gL);
+		
+		String[] buttons = {
+				"cls", "bck", "", "close", "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+"
+		};
+		
+		Text text = new Text(shell, SWT.SINGLE);
+		GridData gd = new GridData();
+		gd.horizontalSpan = 4;
+		gd.horizontalAlignment = GridData.FILL;
+		text.setLayoutData(gd);
+		
+		for (int i = 0; i < buttons.length; i++) {
+			if (i == 2) {
+				Label lbl = new Label(shell, SWT.CENTER);
+				GridData gd1 = new GridData(SWT.FILL, SWT.FILL, false, false);
+				lbl.setLayoutData(gd1);
+			} else {
+				Button button = new Button(shell, SWT.PUSH);
+				button.setText(buttons[i]);
+				GridData gd1 = new GridData(SWT.FILL, SWT.FILL, false, false);
+				gd1.widthHint = 50;
+				gd1.heightHint = 30;
+				button.setLayoutData(gd1);
+			}
+		}
+		shell.setText("calculator");
+		shell.pack();
+		shell.open();
+		
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+
+		Display display = new Display();
+		Calculator_with_GridLayout ex = new Calculator_with_GridLayout(display);
+		display.dispose();
+	}
+
+}
